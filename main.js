61eb13605b046315773e215b8b3500710131eee4
Jonas Holbech <jh@jonasholbech.dk>
2020-03-13 22:54:44 +0100

    done

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.querySelector("h1").textContent = "Here's Jonas";
+const hidden = document.querySelectorAll(".initiallyHidden");
+
+hidden.forEach(h => (h.style.visibility = "hidden"));
+window.addEventListener("keyup", e => {
+  if (e.key === " ") {
+    const el = [...hidden].find(h => h.style.visibility == "hidden");
+    if (el) {
+      el.style.visibility = "visible";
+    }
+  }
+});
